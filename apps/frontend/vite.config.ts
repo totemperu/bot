7c6e7c19c380008c8e37a9f6f48d1a54cae5039b
David Duran (work) <20960328+totallynotdavid@users.noreply.github.com>
2025-12-19 09:43:01 -0500

    add devtools plugin for vite

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,15 @@
+import devtoolsJson from "vite-plugin-devtools-json";
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [tailwindcss(), sveltekit()],
+    plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+
     server: {
         proxy: {
             "/api": "http://localhost:3000",
-            "/static": "http://localhost:3000",
-        },
-    },
+            "/static": "http://localhost:3000"
+        }
+    }
 });
